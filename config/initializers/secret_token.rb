831056d6e9da610d8f7349721eec360bbdbaa7e9
Stefan Breunig <stefan-github@yrden.de>
2013-12-09 13:47:56 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hipsterpizza::Application.config.secret_key_base = 'ce3d4fd3a57914c6c53d40ae7f1b664bf9c273abfad12a2bf4856f0b529ebcc66121e547827306a06b152c5d06a68d2aec0aa49ea3b7e1e2761fdd560ca1dd73'
