747a4328fbc37fe6fe6c35f05abbaadf2fa77662
Stefan Breunig <stefan-github@yrden.de>
2014-01-28 21:30:54 +0100

    unify cookie handling

---
+++ app/helpers/cookie_helper.rb
@@ -0,0 +1,26 @@
+module CookieHelper
+  def cookie_set(key, value, perma = true)
+    raise "invalid cookie key" unless cookie_valid_key?(key)
+
+    key = "_hipsterpizza_#{key}"
+    if value.nil?
+      cookies.delete key
+    else
+      c = perma ? cookies.permanent : cookies
+      c[key] = value
+    end
+  end
+
+  def cookie_get(key)
+    raise "invalid cookie key" unless cookie_valid_key?(key)
+    cookies["_hipsterpizza_#{key}"]
+  end
+
+  def cookie_delete(key)
+    cookie_set(key, nil)
+  end
+
+  def cookie_valid_key?(key)
+    ["basket", "admin", "action", "nick", "order"].include?(key.to_s)
+  end
+end
