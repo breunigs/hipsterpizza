7560aaa79cd80d3be8aa03b011ef3687d7618c66
Stefan Breunig <stefan-github@yrden.de>
2014-01-29 13:56:39 +0100

    order details renderer

--- app/helpers/basket_helper.rb
+++ app/helpers/basket_helper.rb
@@ -2,4 +2,14 @@ module BasketHelper
   def admin?
     @basket && @basket.uid == cookie_get(:basket).to_s
   end
+
+  def order_details(order)
+    content_tag(:ul) do
+      order.json_parsed.each do |item|
+        ingred = item["extra"] * " + "
+        ingred = ingred.empty? ? "" : " + #{ingred}"
+        concat(content_tag(:li, "#{item["prod"]}#{ingred}"))
+      end
+    end
+  end
 end
