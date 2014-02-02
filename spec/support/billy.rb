f6436e7f1144700dedbf47ee973ac2ae77a59435
Stefan Breunig <stefan-github@yrden.de>
2014-02-02 21:01:38 +0100

    fix caching for certain URLs

--- spec/support/billy.rb
+++ spec/support/billy.rb
@@ -7,10 +7,14 @@
 module Billy
   class Cache
     def cacheable?(url, headers)
-      if Billy.config.cache
-        url = URI(url)
-        !url.path.start_with?('/hipster')
-      end
+      return false unless Billy.config.cache
+      url = URI(url)
+      return false if url.path.start_with?('/hipster')
+      # new and edit order share the same URL and are discerned by
+      # cookies only. Caching would thus incorrectly handle replay data.
+      return false if url.query.include?('knddomain=1') rescue false
+
+      true
     end
   end
 end
@@ -21,7 +25,11 @@ Billy.configure do |c|
     'http://stats.g.doubleclick.net/__utm.gif',
     'http://logc279.xiti.com/hit.xiti',
     'https://apis.google.com/_/+1/fastbutton',
-    'https://accounts.google.com/o/oauth2/postmessageRelay'
+    'https://accounts.google.com/o/oauth2/postmessageRelay',
+    # the same store is used for all tests, so the params do not matter
+    # for this
+    'http://localhost/_shop/shopinit_json',
+    'http://127.0.0.1/_shop/shopinit_json'
   ]
   c.persist_cache = true
   c.cache_path = 'tmp/billy-reqs/'
