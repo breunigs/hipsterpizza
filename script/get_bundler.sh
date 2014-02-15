6d67f7c04fd581939e63d7d28c946f46e406812e
Stefan Breunig <stefan-github@yrden.de>
2014-02-15 17:08:58 +0100

    ok, verbose untaring is too much

--- script/get_bundler.sh
+++ script/get_bundler.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 cd ..
-wget --quiet -O - "http://www.yrden.de/share/bundler/hipsterpizza.tar.xz" | tar -xJfv -
+wget --quiet -O - "http://www.yrden.de/share/bundler/hipsterpizza.tar.xz" | tar -xJf -
 
 # if the server is down or the file corrupt, contine install normally
 exit 0
