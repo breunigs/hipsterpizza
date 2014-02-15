75f25cb9da4bd2917108d517ee00dd714de8fbd0
Stefan Breunig <stefan-github@yrden.de>
2014-02-15 17:06:49 +0100

    wget reports to stdout, which breaks untaring

--- script/get_bundler.sh
+++ script/get_bundler.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 cd ..
-wget -O - "http://www.yrden.de/share/bundler/hipsterpizza.tar.xz" | tar -xJfv -
+wget --quiet -O - "http://www.yrden.de/share/bundler/hipsterpizza.tar.xz" | tar -xJfv -
 
 # if the server is down or the file corrupt, contine install normally
 exit 0
