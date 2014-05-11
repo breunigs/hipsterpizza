4d4f4a721afeb8eb04542ea1bffe7cf313b7404b
Stefan Breunig <stefan-github@yrden.de>
2014-05-11 11:09:51 +0200

    remove more files when pushing bundle packages

--- script/push_bundler.zsh
+++ script/push_bundler.zsh
@@ -12,6 +12,7 @@ rvm use 1.9.3
 bundle install --without development --deployment --jobs 4
 
 rm -rf vendor/bundle/**/gems/nokogiri-*/ext/nokogiri/tmp
+rm -rf vendor/bundle/**/gems/nokogiri-*/ports/*/libxml2/*/share/*doc
 rm -rf vendor/bundle/**/gems/capybara-webkit-*/src/webkit_server.gch/c++
 tar cf pack_bundler.tar vendor/bundle
 xz --verbose -9e pack_bundler.tar
