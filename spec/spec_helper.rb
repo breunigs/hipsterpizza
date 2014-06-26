ec65258ea9b70fcc7a6eee7d156de830cc559ce5
Stefan Breunig <stefan-github@yrden.de>
2014-06-26 11:02:09 +0200

    style nits

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,21 @@
 # encoding: utf-8
 
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'billy/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
-    # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
@@ -29,8 +29,7 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = "random"
-
+  config.order = 'random'
 
   config.before(:each) do
     if Capybara && Capybara.current_driver.to_s.include?('no_imgs')
@@ -38,7 +37,6 @@ RSpec.configure do |config|
     end
   end
 end
-
 
 # Better alternative to database cleaner, see
 # http://blog.plataformatec.com.br/tag/capybara/
