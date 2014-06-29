433488d0b86ddadd8a366eb6dcdf92f38b1055b9
Stefan Breunig <stefan-github@yrden.de>
2014-06-29 20:32:00 +0200

    test BasketController#find_changes

---
+++ spec/controllers/basket_controller_spec.rb
@@ -0,0 +1,65 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe BasketController do
+  describe '#find_changes' do
+    before(:all) do
+      BC = BasketController.new
+      @basket = Basket.new
+      BC.instance_variable_set(:@basket, @basket)
+    end
+
+    before do
+      @basket.updated_at = Time.now
+      BC.params = {}
+    end
+
+    it 'reports newer @basket timestamp as change' do
+      BC.params = { ts_basket: (Time.now - 1.minute).to_i }
+      BC.send(:find_changes)
+      expect(BC.instance_variable_get(:@basket_changed)).to be_true
+    end
+
+    it 'doesn’t report equal @basket timestamp as change' do
+      BC.params = { ts_basket: @basket.updated_at.to_i }
+      BC.send(:find_changes)
+      expect(BC.instance_variable_get(:@basket_changed)).to be_false
+    end
+
+    it 'doesn’t report older @basket timestamp as change' do
+      BC.params = { ts_basket: (Time.now + 1.minute).to_i }
+      BC.send(:find_changes)
+      expect(BC.instance_variable_get(:@basket_changed)).to be_false
+    end
+
+    it 'reports deleted @order as change' do
+      BC.params = { ts_order: Time.now.to_i }
+      BC.send(:find_changes)
+      expect(BC.instance_variable_get(:@order_changed)).to be_true
+    end
+
+    context 'with @order' do
+      before(:all) do
+        @order = Order.new
+        BC.instance_variable_set(:@order, @order)
+      end
+
+      before do
+        @order.updated_at = Time.now
+      end
+
+      it 'reports new @order as change' do
+        BC.send(:find_changes)
+        expect(BC.instance_variable_get(:@order_changed)).to be_true
+      end
+
+
+      it 'doesn’t report older @order timestamp as change' do
+        BC.params = { ts_order: (Time.now + 1.minute).to_i }
+        BC.send(:find_changes)
+        expect(BC.instance_variable_get(:@order_changed)).to be_false
+      end
+    end
+  end
+end
