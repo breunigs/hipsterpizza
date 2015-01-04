breunigs/hipsterpizza

commit 6b69b08b96697f1e0a654764481da45847b4e89a

address deprecation warnings

db/migrate/20131229003444_create_orders.rb
@@ -7,7 +7,7 @@ class CreateOrders < ActiveRecord::Migration
       t.integer :basket_id
       t.boolean :paid, default: false
 
-      t.timestamps
+      t.timestamps, null: false
     end
   end
 end

db/migrate/20131229003948_create_baskets.rb
@@ -4,7 +4,7 @@ class CreateBaskets < ActiveRecord::Migration
       t.string :shop_name
       t.datetime :submitted
 
-      t.timestamps
+      t.timestamps, null: false
     end
   end
 end

commit 31a528d73a539088c54819628f0b056ea4892bdd

fix syntax error introduced by 6b69b08b96697f1e0a654764481da45847b4e89a

db/migrate/20131229003444_create_orders.rb
@@ -7,7 +7,7 @@ class CreateOrders < ActiveRecord::Migration
       t.integer :basket_id
       t.boolean :paid, default: false
 
-      t.timestamps, null: false
+      t.timestamps null: false
     end
   end
 end

db/migrate/20131229003948_create_baskets.rb
@@ -4,7 +4,7 @@ class CreateBaskets < ActiveRecord::Migration
       t.string :shop_name
       t.datetime :submitted
 
-      t.timestamps, null: false
+      t.timestamps null: false
     end
   end
 end

db/schema.rb
@@ -13,11 +13,11 @@
 
 ActiveRecord::Schema.define(version: 20141027212153) do
 
-  create_table "baskets", force: true do |t|
+  create_table "baskets", force: :cascade do |t|
     t.string   "shop_name"
     t.datetime "submitted"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "shop_url"
     t.string   "uid"
     t.boolean  "cancelled",       default: false
@@ -27,17 +27,17 @@ ActiveRecord::Schema.define(version: 20141027212153) do
     t.string   "shop_url_params"
   end
 
-  create_table "orders", force: true do |t|
+  create_table "orders", force: :cascade do |t|
     t.string   "nick"
     t.string   "json"
     t.string   "uuid"
     t.integer  "basket_id"
     t.boolean  "paid",       default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
-  create_table "saved_orders", force: true do |t|
+  create_table "saved_orders", force: :cascade do |t|
     t.string "shop_url"
     t.string "nick"
     t.string "json"