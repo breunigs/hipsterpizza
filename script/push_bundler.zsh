3ca64c2dccc9262851c879eab768fc97a9ada4e3
Stefan Breunig <stefan-github@yrden.de>
2015-01-11 21:12:18 +0100

    fix gem package generator script

--- script/push_bundler.zsh
+++ script/push_bundler.zsh
@@ -5,16 +5,20 @@ cd ..
 
 source ~/.rvm/scripts/rvm
 
-rvm use 2.1.2
+rvm use $(cat .ruby-version)
 bundle install --without development --deployment --jobs 4
 
 rm -rf vendor/bundle/**/cache/*.gem
 rm -rf vendor/bundle/**/nokogiri-*
-rm -rf vendor/bundle/**/capybara-webkit-*
 tar cf pack_bundler.tar vendor/bundle
 
 # reset to dev mode
-bundle install --no-deployment
+rm .bundle/config
+rm -rf vendor/bundle
+bundle install --no-deployment --jobs 4&
 
 xz --verbose -9e pack_bundler.tar
 rsync --partial --progress "./pack_bundler.tar.xz" vollmar-stefan:/srv/trap/share/bundler/hipsterpizza.tar.xz
+
+rm pack_bundler.tar.xz
+wait
