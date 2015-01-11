08a6a0b3ddc62b15014921fbb64ce36aa3f76d3f
Stefan Breunig <stefan-github@yrden.de>
2015-01-11 11:42:47 +0100

    add pending tests for basket helper

---
+++ spec/helpers/basket_helper_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe BasketHelper, :type => :helper do
+  describe '#update_via_js' do
+    pending
+  end
+
+  describe '#order_details' do
+    pending
+  end
+
+  describe '#time' do
+    pending
+  end
+
+  describe '#contact_field' do
+    pending
+  end
+
+  describe '#contact_sha_address' do
+    pending
+  end
+
+  describe '#contact_details_array' do
+    pending
+  end
+
+  describe '#order_details_array' do
+    pending
+  end
+end
