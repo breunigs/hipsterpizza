d55de011d56461f0c6cf8a2bcca369eda3f83d88
Stefan Breunig <stefan-github@yrden.de>
2015-01-11 11:43:02 +0100

    cleanup

--- spec/controllers/basket_submit_controller_spec.rb
+++ spec/controllers/basket_submit_controller_spec.rb
@@ -69,29 +69,3 @@ describe BasketSubmitController, type: :controller do
     end
   end
 end
-
-
-
-# class BasketSubmitController < ApplicationController
-#   include CookieHelper
-#   include ActionController::Live
-
-#   before_filter :require_basket, only: :submit
-#   before_filter :load_config
-
-#   def submit
-#     @basket.update_attribute(:submitted, Time.now)
-#     provider = @cfg['order_by_fax'] ? @cfg['fax_provider'] : nil
-
-#     case provider
-#     when 'manual' then
-#       redirect_to pdf_basket_path(@basket.uid)
-#     when 'pdf24' then
-#       stream('submit_fax_pdf24')
-#     else
-#       cookie_set(:replay, "basket #{replay_mode} #{@basket.uid}")
-#       cookie_set(:mode, :pizzade_basket_submit)
-#       redirect_to_shop
-#     end
-#   end
-# end
